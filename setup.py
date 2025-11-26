abb0df86bfac746dc95df207c4de6cd06e136655
41cha <vovaa.dzimina@gmail.com>
2025-11-26 17:21:11 +0200

    add new version 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="linux_commands",
-    version="0.2",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=[
         # список залежностей вашого пакету, якщо вони є
