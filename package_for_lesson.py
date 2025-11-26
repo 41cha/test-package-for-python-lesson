eec7871ec899be4f7d56c8d08a45616a87bc80dc
41cha <vovaa.dzimina@gmail.com>
2025-11-26 15:01:33 +0200

    add funcs to package

---
+++ package_for_lesson.py
@@ -0,0 +1,39 @@
+import os
+
+
+def ls(directory, flag='-l'):
+    for item in os.listdir(directory):
+        print(item)
+
+def pwd():
+    print(os.getcwd())
+
+def mkdir(directory_name):
+    os.mkdir(directory_name)
+
+def rm(file_or_directory):
+    os.remove(file_or_directory)
+
+def cat(filename):
+    if os.path.exists(filename):
+        with open(filename, "r") as file:
+            content = file.read()
+            print(content)
+    else:
+        print("Файл не знайдено.")
+
+    
+def cp(sourse, destination):
+    pass
+
+def mv(sourse, destination):
+    os.rename(sourse, destination)
+
+
+print('--------------------------------------------------------------------------------')
+ls(r'C:\Users\intit\Documents\test_for_package')
+print('--------------------------------------------------------------------------------')
+cat(r'C:\Users\intit\Documents\test_for_package\file.txt')
+print('--------------------------------------------------------------------------------')
+pwd()
+print('--------------------------------------------------------------------------------')
