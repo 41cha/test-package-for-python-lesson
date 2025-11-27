dbab23933539732ec5cfeb44e3f3e580165b2dd0
41cha <vovaa.dzimina@gmail.com>
2025-11-27 17:18:03 +0200

    add new version

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="directory_control",
-    version="0.1.0.dev1",
+    name="DirectoryControl",
+    version="0.1.0.1",
     packages=find_packages(),
     install_requires=[
         # список залежностей вашого пакету, якщо вони є
@@ -10,6 +10,6 @@ setup(
     author="Volodymyr Dzimina",
     author_email="vova.dzimina@gmail.com",
     description="This is a test package for python lessons",
-    long_description="This package created for my python lessons with Dmitro Probachai, in this package i created simple linux comands",
+    long_description="This package created for my python lessons with Dmitro Probachai, in this package i added simple linux comands",
     url="https://github.com/41cha/test-package-for-python-lesson",
 )
